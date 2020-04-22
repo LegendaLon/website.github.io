3a38a3ffa4ee9828e5f2a1afd6dd86b6fc464e4e
LegendaLon <LegendaLon@users.noreply.github.com>
2020-04-22 16:27:15 +0300

    2

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 from app import app
-from flask import Flask, request, render_template
+from flask import Flask, request
+from flask import render_template as render
+from flask import request, redirect
 
 @app.route("/", methods=['GET'])
 def index():
-	names = "Legenda Lon"
-	return render_template('index.html', n=names)
+	names = "Чёрный дух"
+	username = request.cookies.get('username')
+	data = (names, username)
+	return render('index.html', data=data)
+
+@app.route("/home", methods=['GET', 'POST'])
+def home():
+	return render('home.html')
+
+@app.route('/forward/', methods=["POST"])
+def forwaed(name):
+	a = "Hello" + name
+	return render('home1.html', a=a)
+
+def move_forward():
+	print("Hello")
 
 if __name__ == "__main__":
 	app.run(debug=True)
